5e71e5e2e020788ffc007176f3070509445f1585
LaulinVasquez <427339400@byui.edu>
2025-03-09 20:20:24 -0600

    css friendly design finished

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const modal = document.querySelector("#modal");
+const modal = document.querySelector("#modal"); 
 const formUsername = document.querySelector("#formUsername");
 
 formUsername.addEventListener("submit", (form) => {
